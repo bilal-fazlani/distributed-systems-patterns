0bf5b737aeb625d39ebbee5068312bc335e648f2
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2024-02-07 20:24:02 +0530

    add demo sse stream endpoint

--- src/main/scala/com/bilalfazlani/3-log-snapshots/httpapp/KVEndpoints.scala
+++ src/main/scala/com/bilalfazlani/3-log-snapshots/httpapp/KVEndpoints.scala
@@ -5,6 +5,7 @@ import zio.http.*
 import zio.http.endpoint.*
 import zio.*
 import zio.schema.*
+import zio.stream.ZStream
 
 case class KeyNotFound(key: String) derives Schema
 
@@ -19,13 +20,25 @@ val getEndpoint =
     .outError[KeyNotFound](Status.NotFound)
 
 val getRoute =
-
   def handler(k: String): ZIO[Any, KeyNotFound, String] =
     ZIO.fromOption(mutableMap.get(k)).mapError(_ => KeyNotFound(k))
-
   getEndpoint.implement(Handler.fromFunctionZIO[String](handler))
 
 val putRoute = put.implement(Handler.fromFunction(mutableMap.put.apply))
 val getAllRoute = getAll.implement(Handler.fromFunction { _ => mutableMap.toMap })
 
-val allRoutes = Routes(getRoute, putRoute, getAllRoute)
+val streamEndpoint = Endpoint(Method.GET / "stream").outStream[ServerSentEvent]
+val streamRoute = streamEndpoint.implement(
+  Handler.fromFunction(_ =>
+    ZStream
+      .range(0, 3)
+      .tap(_ => ZIO.sleep(1.second))
+      .map(i => ServerSentEvent(i.toString, None, None, None))
+  )
+)
+
+val allRoutes = Routes(streamRoute, getRoute, putRoute, getAllRoute)
+
+enum Event derives Schema:
+  case Put(key: String, value: String)
+  case Delete(key: String)
